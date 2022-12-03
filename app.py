92a8b8b91ebf5ee1b5a3b9548f09f2b66f51bdd0
Bruno Zeppa Garcia <bruno.z@tugerente.com>
2022-12-02 20:15:48 -0600

    Fisrt python commit :)

--- app.py
+++ app.py
@@ -3,23 +3,25 @@ from flask_api import status
 import requests
 import os
 
-ENDPOINT_POKE_API = "https://pokeapi.co/api/v2/pokemon/ditto"
-response = requests.get(ENDPOINT_POKE_API)
-response = response.json()
-
-abilities = response["abilities"][0]
-ability_name = abilities["ability"]["name"]
-
 
 app = Flask(__name__)
 
 
-@app.route("/api/v2/poke/<abilit_I_wish>")
-def poke(abilit_I_wish):
-    if abilit_I_wish in ability_name:
-        return {"status": "success", "message": "you find limber ability!", "data": abilities}, 200
-    else:
-        return "limber is not here"
+@app.route("/api/v2/poke")
+def poke():
+
+    endpoint_poke_api = request.headers.get("endpoint_poke_api")
+    ability_name = request.headers.get("ability_name")
+
+    response = requests.get(str(endpoint_poke_api))
+    response = response.json()
+    abilities = response["abilities"]
+
+    for ability in abilities:
+        if ability_name == ability["ability"]["name"]:
+            return {"status": "success", "message": "This pokemon has the ability!", "data": abilities}, 200
+        else:
+            return {"status": "not found", "message": "This pokemon hasn't the ability"}, 404
 
 
 @app.route("/api/v2/status")
